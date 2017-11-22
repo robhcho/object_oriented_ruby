a1a3044db36c303f9087e89993e4f4dfe72cbb31
Rob Cho <robhcho@gmail.com>
2017-11-21 22:35:59 -0500

    Add Vehicle class to inherit from

---
+++ inheritance_example.rb
@@ -0,0 +1,45 @@
+class Vehicle
+ def initialize
+    @speed = 0
+    @direction = 'north'
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car < Vehicle
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+
+  # def show_info
+  #   "#{}"
+  # end
+end
+
+class Bike < Vehicle
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
+
+
+car = Car.new
+bike = Bike.new
+p car.accelerate
+p car.honk_horn
+p bike.accelerate
+p bike.ring_bell
+
+
+
